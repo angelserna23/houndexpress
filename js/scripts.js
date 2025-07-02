35f8c00d04c5116e09694369f9a74977f8bdec1a
angelserna23 <andres.hdzserna@gmail.com>
2025-07-01 21:47:54 -0600

    second trial adding variable to DOM

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,27 @@ const destinatary = document.querySelector(".destinatary");
 const origin = document.querySelector(".origin");
 const destiny = document.querySelector(".destiny");
 const date = document.querySelector(".date");
-const status = document.querySelector(".status");
+const statusGuide = document.querySelector(".form__select-status");
 const registerBtn = document.querySelector(".form__send")
+const form = document.querySelector(".form");
+
+const data = []; //Aqui vamos agregando toda la informacion de la guia
+
+function registerGuide() {
+    const infoGuide = {
+        number: guideNumber.value.trim(),
+        destinatary: destinatary.value.trim(),
+        origin: origin.value.trim(),
+        destiny: destiny.value.trim(),
+        date: date.value,
+        status: statusGuide.value
+    };
+
+    data.push(infoGuide);
+}
+
+//Aqui le decimos que en el evento "submit" se agreguen los datos a nuestro array "data"
+form.addEventListener('submit', (e) => {
+    e.preventDefault;
+    registerGuide();
+});
