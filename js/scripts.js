3342f3113662f422db11f8bc35a243d0991f50d7
angelserna23 <andres.hdzserna@gmail.com>
2025-07-07 22:06:50 -0600

    start with modifications for list.html

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,18 @@ const statusGuide = document.querySelector(".form__select-status");
 const registerBtn = document.querySelector(".form__send")
 const form = document.querySelector(".form");
 
-const data = []; //Aqui vamos agregando toda la informacion de la guia
+/*Esta funcion nos permite agregar nuestros arrays declarados dentro de nuestro data*/
+const storeGuides = JSON.parse(localStorage.getItem("guides")) || [];
+const data = [...storeGuides]; //Aqui vamos agregando toda la informacion de la guia
 
+/*Esta funcion nos permite darnos un peview de lo que acabamos de generar en nuestro register.html*/
 function registerGuide() {
+
+    if(!infoGuide.number || !infoGuide.destinatary || !infoGuide.origin || !infoGuide.destiny || !infoGuide.date  || !infoGuide.status) {
+        alert("Favor de ingresar todos los datos el formulario");
+        return;
+    };
+
     const infoGuide = {
         number: guideNumber.value.trim(),
         destinatary: destinatary.value.trim(),
@@ -29,30 +38,23 @@ function registerGuide() {
 
     data.push(infoGuide);
 
-    if(!infoGuide.number || !infoGuide.destinatary || !infoGuide.origin || !infoGuide.destiny || !infoGuide.date  || !infoGuide.status) {
-        alert("Favor de ingresar todos los datos el formulario");
-        return;
-    };
-
+    /*-----------Muy Importante-----------*/
+    localStorage.setItem("guides", JSON.stringify(data)); // Aqui guardamos la informacion en nuestro localStorage
 
     /*Aqui creamos el section que va a ser para la tarjeta*/
     const createSection = document.createElement("section");
-    createSection.setAttribute("class","guides__info");
     createSection.classList.add("guides__info");
 
     /*-----------------Creacion del numero de guia----------------------*/
     //Vamos a agregar los div's
     const containerDivNumber = document.createElement("div");
-    containerDivNumber.setAttribute("class", "guides__container");
     containerDivNumber.classList.add("guides__container");
 
     const titleAddNumber = document.createElement("h2");
-    titleAddNumber.setAttribute("class","guides__info-title");
     titleAddNumber.classList.add("guides__info-title");
     titleAddNumber.textContent = "Numero de guia:"; //Aqui le agregamos el nombre de nuestro titulo
 
     const textAddNumber = document.createElement("p");
-    textAddNumber.setAttribute("class","guides__info-text");
     textAddNumber.classList.add("guides__info-text");
     textAddNumber.textContent = infoGuide.number; //Aqui agregamos la key que guardamos dentro de nuestro objeto "infoGuide"
 
@@ -64,16 +66,13 @@ function registerGuide() {
     /*-----------------Creacion del destinatario----------------------*/
     //Vamos a agregar los div's
     const containerDivDestinatary = document.createElement("div");
-    containerDivDestinatary.setAttribute("class", "guides__container");
     containerDivDestinatary.classList.add("guides__container");
 
     const titleAddDestinatary = document.createElement("h2");
-    titleAddDestinatary.setAttribute("class","guides__info-title");
     titleAddDestinatary.classList.add("guides__info-title");
     titleAddDestinatary.textContent = "Destinatario:"; //Aqui le agregamos el nombre de nuestro titulo
 
     const textAddDestinatary = document.createElement("p");
-    textAddDestinatary.setAttribute("class","guides__info-text");
     textAddDestinatary.classList.add("guides__info-text");
     textAddDestinatary.textContent = infoGuide.destinatary; //Aqui agregamos la key que guardamos dentro de nuestro objeto "infoGuide"
 
@@ -85,16 +84,13 @@ function registerGuide() {
     /*-----------------Creacion del Origen----------------------*/
     //Vamos a agregar los div's
     const containerDivOrigin = document.createElement("div");
-    containerDivOrigin.setAttribute("class", "guides__container");
     containerDivOrigin.classList.add("guides__container");
 
     const titleAddOrigin = document.createElement("h2");
-    titleAddOrigin.setAttribute("class","guides__info-title");
     titleAddOrigin.classList.add("guides__info-title");
     titleAddOrigin.textContent = "Origen:"; //Aqui le agregamos el nombre de nuestro titulo
 
     const textAddOrigin = document.createElement("p");
-    textAddOrigin.setAttribute("class","guides__info-text");
     textAddOrigin.classList.add("guides__info-text");
     textAddOrigin.textContent = infoGuide.origin; //Aqui agregamos la key que guardamos dentro de nuestro objeto "infoGuide"
 
@@ -106,16 +102,13 @@ function registerGuide() {
     /*-----------------Creacion del Destino----------------------*/
     //Vamos a agregar los div's
     const containerDivDestiny = document.createElement("div");
-    containerDivDestiny.setAttribute("class", "guides__container");
     containerDivDestiny.classList.add("guides__container");
 
     const titleAddDestiny = document.createElement("h2");
-    titleAddDestiny.setAttribute("class","guides__info-title");
     titleAddDestiny.classList.add("guides__info-title");
     titleAddDestiny.textContent = "Destino:"; //Aqui le agregamos el nombre de nuestro titulo
 
     const textAddDestiny = document.createElement("p");
-    textAddDestiny.setAttribute("class","guides__info-text");
     textAddDestiny.classList.add("guides__info-text");
     textAddDestiny.textContent = infoGuide.destiny; //Aqui agregamos la key que guardamos dentro de nuestro objeto "infoGuide"
 
@@ -127,16 +120,13 @@ function registerGuide() {
     /*-----------------Creacion de la fecha----------------------*/
     //Vamos a agregar los div's
     const containerDivDate = document.createElement("div");
-    containerDivDate.setAttribute("class", "guides__container");
     containerDivDate.classList.add("guides__container");
 
     const titleAddDate = document.createElement("h2");
-    titleAddDate.setAttribute("class","guides__info-title");
     titleAddDate.classList.add("guides__info-title");
     titleAddDate.textContent = "Destino:"; //Aqui le agregamos el nombre de nuestro titulo
 
     const textAddDate = document.createElement("p");
-    textAddDate.setAttribute("class","guides__info-text");
     textAddDate.classList.add("guides__info-text");
     textAddDate.textContent = infoGuide.date; //Aqui agregamos la key que guardamos dentro de nuestro objeto "infoGuide"
 
@@ -148,16 +138,13 @@ function registerGuide() {
     /*-----------------Creacion de la status----------------------*/
     //Vamos a agregar los div's
     const containerDivStatus = document.createElement("div");
-    containerDivStatus.setAttribute("class", "guides__container");
     containerDivStatus.classList.add("guides__container");
 
     const titleAddStatus = document.createElement("h2");
-    titleAddStatus.setAttribute("class","guides__info-title");
     titleAddStatus.classList.add("guides__info-title");
     titleAddStatus.textContent = "Estado:"; //Aqui le agregamos el nombre de nuestro titulo
 
     const textAddStatus = document.createElement("p");
-    textAddStatus.setAttribute("class","guides__info-text");
     textAddStatus.classList.add("guides__info-text");
     textAddStatus.textContent = infoGuide.status; //Aqui agregamos la key que guardamos dentro de nuestro objeto "infoGuide"
 
@@ -183,3 +170,22 @@ if(registerBtn){
         registerGuide();
     });
 };
+
+/*-----------------Registro de datos-------------------*/
+/*
+Aqui vamos a mover los datos de:
+    register.html --> list.html
+*/
+
+function insertGuides() {
+    const storeGuides = JSON.parse(localStorage.getItem("guides")) || [];
+
+    storeGuides.forEach(guide => {
+        
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    insertGuides();
+});
+
