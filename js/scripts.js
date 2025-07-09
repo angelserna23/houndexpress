916aa8e5c348a818571e555174248ae35161d52c
angelserna23 <andres.hdzserna@gmail.com>
2025-07-08 21:26:33 -0600

    creation of the function to add rows to the table

--- js/scripts.js
+++ js/scripts.js
@@ -171,6 +171,13 @@ if(registerBtn){
     });
 };
 
+
+
+
+
+
+
+
 /*-----------------Registro de datos-------------------*/
 /*
 Aqui vamos a mover los datos de:
@@ -181,6 +188,88 @@ function insertGuides() {
     const storeGuides = JSON.parse(localStorage.getItem("guides")) || [];
 
     storeGuides.forEach(guide => {
+        /*-----Aqui comenzamos creando la fila para la tabla-----*/
+        const row = document.createElement("tr");
+        row.classList.add("list__table-body-row")
+
+        //Agregar el numero de serie
+        const rowGuide = document.createElement("td");
+        rowGuide.classList.add("list__table-body-row-info");
+        rowGuide.textContent = guide.number;
+
+        row.appendChild(rowGuide);
+
+        //Agregar el destinatario
+        const rowDestinatary = document.createElement("td");
+        rowDestinatary.classList.add("list__table-body-row-info");
+        rowDestinatary.textContent = guide.destinatary
+
+        row.appendChild(rowDestinatary);
+
+        //Agregar el origen
+        const rowOrigin = document.createElement("td");
+        rowOrigin.classList.add("list__table-body-row-info");
+        rowOrigin.textContent = guide.origin;
+
+        row.appendChild(rowOrigin);
+
+        //Agregar la fecha de creacion
+        const rowDateCreation = document.createElement("td");
+        rowDateCreation.classList.add("list__table-body-row-info");
+        rowDateCreation.textContent = guide.date;
+
+        row.appendChild(rowDateCreation);
+
+        //Agregar la fecha de ultima modificacion
+        const rowDateModification = document.createElement("td");
+        rowDateModification.classList.add("list__table-body-row-info");
+
+        const updateDate = guide.history[guide.history.length - 1].date;
+        rowDateModification.textContent = updateDate;
+
+        row.appendChild(rowDateModification);
+
+        //Agregar el estatus
+        const rowStatusModification = document.createElement("td");
+        rowStatusModification.classList.add("list__table-body-row-info")
+
+        const updateStatus = guide.history[guide.history.length - 1].status;
+        rowStatusModification.textContent = updateStatus;
+
+        row.appendChild(rowStatusModification);
+
+        //Agregar el boton de actualizar
+        const rowButton = document.createElement("td");
+        rowButton.classList.add("list__table-body-row-info");
+
+        const addButton = document.createElement("button");
+        addButton.classList.add("list__table-body-row-info--button");
+        addButton.textContent = "Actualizar";
+
+        rowButton.appendChild(addButton);
+
+        row.appendChild(rowButton);
+
+        //Agregar la opcion de desplegar las ultimas modifaciones
+        const rowHistory = document.createElement("td");
+        rowHistory.classList.add("list__table-body-row-info");
+
+        const spanHistory = document.createElement("span");
+        spanHistory.classList.add("historial");
+        spanHistory.textContent = "Desplegar";
+
+        const imgHistory = document.createElement("img");
+        imgHistory.classList.add("historial__img");
+        imgHistory.src = "img/arrow_down_black.svg";
+        imgHistory.alt = "";
+
+        spanHistory.appendChild(imgHistory);
+        rowHistory.appendChild(spanHistory);
+
+        row.appendChild(rowHistory);
+
+        /*Agregamos toda esta informacion al body de la fila de la tabla*/
+        document.querySelector("list__table-body").appendChild(row);
         
     });
 }
